e7a164bea4b0a2d8a8a65370c74d16d04dea6e9f
Seungyeon Huh <brave.huhsame@gmail.com>
2023-10-25 15:57:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from langchain.chat_models import ChatOpenAI
 import streamlit as st
 
 # Chat Model 초기화
-chat_model = ChatOpenAI(model="gpt-4", temperature=1.06, max_tokens=1024)
+chat_model = ChatOpenAI(model="gpt-4", temperature=1.06, max_tokens=800)
 
 system_message = "지금은 2023년이고 너는 미래 2033년에 살고있어. 인터넷 신문사 '지피터스 타임즈' 3년차 기자 '구라임'이야. 자극적이고 재미있는 기사를 많이 써. 가끔 비난도 해. "
 
@@ -24,7 +24,7 @@ field = st.text_input("전공이나 관심사")
 dream = st.text_input("미래의 나에게 꼭 바라는 것")
 
 if st.button("🪄 미래 알아보기"):
-    with st.spinner("주문을 외우는 중 ... 60~100초"):
+    with st.spinner("주문을 외우는 중 ... 60~120초"):
         # 예측 요청
         human_message = f"다음 나의 정보를 이용해서 니가 살고있는 2033년에서 나에 대한 자극적인 기사를 500자 이내로 작성해줘. 실제 신문기사처럼. \n- 이름: {name}\n- 현재 나이: {age}\n- MBTI: {MBTI}\n- 성별: {sex}\n- 전공이나 관심사: {field}\n- 미래의 나에게 꼭 바라는것: {dream}"
 
